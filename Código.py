4d4a42381674c6ce4425d8ccf37bdd39363cb78d
Leandro da Silva Pereira <leandrodasilvapereira10@gmail.com>
2024-03-23 11:15:05 -0300

    Update Código.py

--- Código.py
+++ Código.py
@@ -1,17 +1,18 @@
 print("########## Par ou Impar ##########")
-#Função para poder chamar em outros momentos
+#Função para poder chamar em outros momentos:
 def par_impar():
     numero=int(input("Digite um número inteiro>> "))
     return numero
-while True: #loop por causa da 3º parte
-    #Chamando a função:
+#loop por causa da 3º parte:
+while True:
+    #Chamando a função para dentro do loop:
     numero_usuario = par_impar()
     #Conta para descobrir se o número é par ou impar
     if numero_usuario % 2 == 0:
-        print("Seu número é: \nPar")
+        print(f"O número {numero_usuario} é: \nPar")
     else:
-        print("Seu número é: \nÍmpar")
-    #3º parte: descobrir se a pessoa quer sair do loop
+        print(f"O número {numero_usuario} é: \nÍmpar")
+    #3º parte: Descobrir se a pessoa quer sair do loop:
     retornar_inicio=input("Deseja testar outro número?(s/n) ")
     if retornar_inicio.lower() != "s" and retornar_inicio.lower() != "n":
         print("Digite apenas 's' ou 'n'")
